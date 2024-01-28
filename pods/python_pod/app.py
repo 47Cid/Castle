156c31ce43172a033dae059840c27e636967bbfc
sid <siddhantkalgutkar@gmail.com>
2024-01-28 18:20:27 -0500

    Added Timestamp to JSON objects

--- pods/python_pod/app.py
+++ pods/python_pod/app.py
@@ -1,9 +1,11 @@
 from flask import Flask, jsonify
 app = Flask(__name__)
+from datetime import datetime
+
 
 @app.route('/verify')
-def hello_world():
-    return jsonify(isValid='True')
+def verify():
+    return jsonify(isValid='True', time=datetime.now().isoformat())
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3031)
